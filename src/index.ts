2264f4c5e93cec446d74e437ab4e123e11e54a65
mesmeri <angels_ua@ukr.net>
2022-06-19 00:54:32 +0300

    feat: create server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import https from "https";
+import "dotenv/config";
+import http from "http";
 
-console.log("started");
+const server = http.createServer((req, res) => {
+  const { method } = req;
+  const baseURL = `http://${req.headers.host}/`;
+
+  res.end(
+    JSON.stringify({
+      success: true,
+      message: `The method is ${method}, base url is ${baseURL}`,
+    })
+  );
+});
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
